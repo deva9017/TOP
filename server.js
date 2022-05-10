b0fc2fd02098e9da9865e6866fc80aaf34c3f2b3
sohailiqrar <sohailiqrar22@gmail.com>
2022-05-11 00:30:07 +0530

    add server.js

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express(); 
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.get("/", function(req, res){
+  res.sendFile(__dirname + "/index.html");
+})
+
+app.listen(3000, function(){
+  console.log("Server is up and running");
+})
