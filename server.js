ea7f6996223c9f4bdb4b214d9add9985556f7f30
sohailiqrar <sohailiqrar22@gmail.com>
2022-05-14 03:47:12 +0530

    complete validation

--- server.js
+++ server.js
@@ -1,15 +1,81 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 
-const app = express(); 
+mongoose.connect("mongodb://localhost:27017/topDB");
+
+const app = express();
 
 app.use(express.static("public"));
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get("/", function(req, res){
+const detailsSchema = new mongoose.Schema({
+  first_name: String,
+  last_name: String,
+  email: String,
+  password: String,
+  dlNumber: String,
+});
+
+const Person = mongoose.model("Person", detailsSchema);
+
+app.get("/", function (req, res) {
   res.sendFile(__dirname + "/index.html");
-})
+});
 
-app.listen(3000, function(){
-  console.log("Server is up and running");
-})
+app.get("/register", function (req, res) {
+  res.sendFile(__dirname + "/public/sign-up.html");
+});
+
+app.post("/register", (req, res) => {
+  let data = {
+    first_name: req.body.fName,
+    last_name: req.body.lName,
+    email: req.body.email,
+    password: req.body.password,
+    dlNumber: req.body.identity,
+  };
+
+  Person.exists(
+    { email: req.body.email, password: req.body.password },
+    function (err, doc) {
+      if (err) console.log(err);
+      else {
+        if (doc) {
+          console.log("Already registered!");
+          res.redirect("/login");
+        } else {
+            let person = new Person(data);
+            person.save();
+            res.redirect("/login");
+        }
+      }
+    }
+  );
+});
+
+app.get("/login", (req, res) => {
+  res.sendFile(__dirname + "/public/sign-in.html");
+});
+
+app.post("/login", (req, res) => {
+  Person.exists(
+    { email: req.body.email, password: req.body.password },
+    function (err, doc) {
+      if (err) console.log(err);
+      else {
+        if (doc) {
+          console.log("Successfully logged in.");
+          res.redirect("/");
+        } else {
+          console.log("Not Registered!");
+          res.redirect("/register");
+        }
+      }
+    }
+  );
+});
+
+app.listen(4000, function () {
+  console.log("Server is up and running at 4000");
+});
